c2d4df6b6fcb2d46bed250c3e5ea83b8cbafb16f
Vaverka123 <maslova.vera.2019@yandex.by>
2025-11-05 15:26:12 +0100

    1. Refaktoryzacja UI

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,15 +6,33 @@ import { NgClass } from '@angular/common';
   imports: [NgClass],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
-  standalone: true
+  standalone: true,
 })
 export class AppComponent {
   title = 'junior-frontend-developer-task';
 
   protected tasks = [
-    { name: 'Zrobić zakupy spożywcze', status: 'Completed', date: '2025-05-01', description: 'Muszę kupić mleko, mąkę i jajka.' },
-    { name: 'Opłacić rachunki', status: 'Pending', date: '2025-05-10', description: 'Tylko nie odkładaj tego na inny dzień!' },
-    { name: 'Urodziny mamy', status: 'Planned', date: '2025-05-15', description: 'Kupić kwiaty i tort.' }
+    {
+      name: 'Zrobić zakupy spożywcze',
+      status: 'Completed',
+      date: '2025-05-01',
+      description: 'Muszę kupić mleko, mąkę i jajka.',
+      descVisible: false,
+    },
+    {
+      name: 'Opłacić rachunki',
+      status: 'Pending',
+      date: '2025-05-10',
+      description: 'Tylko nie odkładaj tego na inny dzień!',
+      descVisible: false,
+    },
+    {
+      name: 'Urodziny mamy',
+      status: 'Planned',
+      date: '2025-05-15',
+      description: 'Kupić kwiaty i tort.',
+      descVisible: false,
+    },
   ];
   protected descVisible: boolean = false;
 
@@ -22,7 +40,7 @@ export class AppComponent {
     task.status = task.status === 'Completed' ? 'Planned' : 'Completed';
   }
 
-  toggleDescription() {
-    this.descVisible = !this.descVisible;
+  toggleDescription(task: any) {
+    task.descVisible = !task.descVisible;
   }
 }
