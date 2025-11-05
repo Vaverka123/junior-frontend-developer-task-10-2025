a7885347b35a05e3f7b00c77ceb7bd27dfc256b0
Vaverka123 <maslova.vera.2019@yandex.by>
2025-11-05 21:08:42 +0100

    2. Filtry

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,19 @@
 import { Component } from '@angular/core';
 import { NgClass } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+interface Task {
+  id: number;
+  name: string;
+  date: string;
+  status: 'Completed' | 'Pending' | 'Planned';
+  description: string;
+  descVisible?: boolean;
+}
 
 @Component({
   selector: 'app-root',
-  imports: [NgClass],
+  imports: [NgClass, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
   standalone: true,
@@ -11,8 +21,9 @@ import { NgClass } from '@angular/common';
 export class AppComponent {
   title = 'junior-frontend-developer-task';
 
-  protected tasks = [
+  protected tasks: Task[] = [
     {
+      id: 1,
       name: 'Zrobić zakupy spożywcze',
       status: 'Completed',
       date: '2025-05-01',
@@ -20,6 +31,7 @@ export class AppComponent {
       descVisible: false,
     },
     {
+      id: 2,
       name: 'Opłacić rachunki',
       status: 'Pending',
       date: '2025-05-10',
@@ -27,6 +39,7 @@ export class AppComponent {
       descVisible: false,
     },
     {
+      id: 3,
       name: 'Urodziny mamy',
       status: 'Planned',
       date: '2025-05-15',
@@ -34,13 +47,31 @@ export class AppComponent {
       descVisible: false,
     },
   ];
-  protected descVisible: boolean = false;
 
-  toggleCompleted(task: any) {
+  filters = {
+    name: '',
+    date: '',
+    status: '',
+  };
+
+  toggleCompleted(task: Task) {
     task.status = task.status === 'Completed' ? 'Planned' : 'Completed';
   }
 
-  toggleDescription(task: any) {
+  toggleDescription(task: Task) {
     task.descVisible = !task.descVisible;
+  }
+
+  filteredTasks(): Task[] {
+    return this.tasks.filter((task) => {
+      const matchesName = task.name
+        .toLowerCase()
+        .includes(this.filters.name.toLowerCase());
+      const matchesDate = !this.filters.date || task.date === this.filters.date;
+      const matchesStatus =
+        !this.filters.status || task.status === this.filters.status;
+
+      return matchesName && matchesDate && matchesStatus;
+    });
   }
 }
